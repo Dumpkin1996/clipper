d40a2a210ecf8aaffdc9028f7267e596ab509909
cypressd1999 <49230186+cypressd1999@users.noreply.github.com>
2019-05-24 17:13:56 +0800

    Add files via upload

---
+++ applications/FatigueDetection/container0/app/predict.py
@@ -0,0 +1,35 @@
+# Import libraries
+import os
+import cv2
+import json
+import random
+import numpy
+
+def image_string(image):
+    image_encode=cv2.imencode('.jpg',image)[1]
+    imagelist=image_encode.tolist()
+    image_string=json.dumps(imagelist)
+    return image_string
+
+def string_image(imagestring):
+    image_list=json.loads(imagestring)
+    arr=np.array(image_list)
+    arr=np.uint8(arr)
+    image=cv2.imdecode(arr,cv2.IMREAD_COLOR)
+    return image
+
+
+def predict(sudostring):
+    filelist=[f for f in os.listdir("part1") if f.endswith(".jpg")]
+    index=random.randint(0,10000)
+    random_image=cv2.imread("part1/"+str(filelist[index]))
+    while random_image is None:
+        index=random.randint(0,10000)
+        random_image=cv2.imread("part1/"+str(filelist[index]))
+    inputstring=image_string(random_image)
+    return inputstring
+
+
+
+
+

680e9eb0bf47fba4282c23fa94db9641372f120e
cypressd1999 <49230186+cypressd1999@users.noreply.github.com>
2019-05-30 14:31:28 +0800

    Update build_docker_FatigueDetection.sh

--- applications/FatigueDetection/build_docker_FatigueDetection.sh
+++ applications/FatigueDetection/build_docker_FatigueDetection.sh
@@ -6,4 +6,5 @@ docker build -f ./FatigueDetection/Dockerfile1 -t detection:container1 .
 docker build -f ./FatigueDetection/Dockerfile2 -t detection:container2 .
 docker build -f ./FatigueDetection/Dockerfile3 -t detection:container3 .
 docker build -f ./FatigueDetection/Dockerfile4 -t detection:container4 .
+docker build -f ./FatigueDetection/Dockerfile5 -t detection:container5 .
 

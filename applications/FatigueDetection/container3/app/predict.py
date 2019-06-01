08e1bca9e6b0dc73b2358af3075062884dba046f
cypressd1999 <49230186+cypressd1999@users.noreply.github.com>
2019-06-01 13:26:43 +0800

    Update predict.py

--- applications/FatigueDetection/container3/app/predict.py
+++ applications/FatigueDetection/container3/app/predict.py
@@ -77,7 +77,7 @@ def predict(imstr):
         r['rois']=np.array([r['rois'][pos]])
         r['scores']=np.array([r['scores'][pos]])
     else:
-        end=timer()
+        end=time.time()
         print("\n[INFO] C3 time:"+str(end-start))
         return None
     prediction=make_box_mask(image, r['rois'].tolist()[0])

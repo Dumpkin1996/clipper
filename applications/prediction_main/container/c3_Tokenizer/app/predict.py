6b9a70259eb890d36ddb2419ac47159e10c0977d
Tianhao Zhang <dumpkin1996@gmail.com>
2019-06-06 23:01:10 +0800

    test

--- applications/prediction_main/container/c3_Tokenizer/app/predict.py
+++ applications/prediction_main/container/c3_Tokenizer/app/predict.py
@@ -1,11 +1,13 @@
 import spacy,en_core_web_sm
 import time
 
+global nlp
+
+nlp = en_core_web_sm.load()
+
 def predict(text_data):
 
 	start = time.time()
-
-	nlp = en_core_web_sm.load()
 
 	doc = nlp(text_data)
 
